f2717378a44e14781e71865c1f344f98e1c24ca3
jenifi_rihab <rihab.jenifi@gmail.fr>
2023-06-22 12:34:52 +0200

    go to the JS

--- 11_map_advanced/starter/main.js
+++ 11_map_advanced/starter/main.js
@@ -11,7 +11,7 @@ async function getUsers() {
             throw new Error('fetch data issue')
         }
      
-        getLastElement(data.results)
+        formatData(data.results)
     } catch (error) {
         console.log(error);
     }
@@ -76,7 +76,41 @@ async function getUsers() {
 
 // 1. afficher un tableau d'objet au format {name : Daphne Durand, age: 61}
 
+            // function formatData(arr){
+            //     const result = arr.map(el =>{
+            //         const first = el.name.first
+            //         const last = el.name.last
+            //         const age= el.dob.age
+            //         return { name: `${el.name.first} ${el.name.last}`, age:el.dob.age}
+                
+            //     });
+            //     console.log(result)
+            // }
+// 2. afficher un tableau d'objet au format {name : Daphne Durand, ageInferiorThirty: true}
 
-// 2. afficher un tableau d'objet au format {name : Daphne Durand, adult: true}
-// 3. afficher un tableau d'objet au format {name : Daphne Durand, adult: true}
-// 4. afficher un tableau de messages au format {mail: 'd.durand@example.com', message: 'Bonjour Daphné, votre adresse "21 rue Voltaire, 33400 Talence" est-elle correcte ?'}
+            // function formatData(arr){
+            //     const result = arr.map(el => {
+            //         const first = el.name.first
+            //         const last = el.name.last
+            //         const res = el.dob.age <= 30
+            //         return { name: `${el.name.first} ${el.name.last}`, ageInferiorThirty:res }
+                    
+            //     })
+            //     console.log(result)
+            //     }
+
+
+// 3. afficher un tableau de messages au format {mail: 'd.durand@example.com', message: 'Bonjour Daphné, votre adresse "21 rue Voltaire, 33400 Talence" est-elle correcte ?'}
+
+function formatData(arr) {
+    const result = arr.map(el => {
+        return {
+            mail: el.email,
+            message: `
+             Bonjour ${el.name.first}, votre adresse "${el.location.street.number} ${el.location.street.name}, ${el.location.postcode} ${el.location.city}" est-elle correcte ?
+             `
+        }
+    });
+
+    console.log(result)
+}
